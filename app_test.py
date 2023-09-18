ca35c5da4b9374277562edf6c96c84c76f2f4bcb
galtoren <galt@wix.com>
2023-09-18 16:46:34 +0300

    [Db_start_up_config] app E2E test file intialized

---
+++ app_test.py
@@ -0,0 +1,34 @@
+import requests
+import unittest
+
+
+class TestFastAPIApp(unittest.TestCase):
+    def setUp(self):
+        self.base_url = "http://0.0.0.0:8000"
+
+    def test_generate_shorter_url(self):
+        url = f"{self.base_url}/generate_shorter_short"
+        data = {
+            "url": "http:",
+        }
+        response = requests.post(url, json=data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_shorter_url(self):
+        generate_url = f"{self.base_url}/generate_shorter_short"
+        data = {
+            "url": "http:",  # TODO: add a long url here and check the logic after implemented
+        }
+        response = requests.post(generate_url, json=data)
+        get_url = f"{self.base_url}/get_shorter_url"
+        generated_url = response.json()  # This will convert the JSON response to a Python dictionary
+        response = requests.get(url=f"{get_url}",
+                                params={'url': generated_url['id']})
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_not_found(self):
+        get_url = f"{self.base_url}/get_shorter_url"
+        url = "https://chat.openai.com/"
+        response = requests.get(url=f"{get_url}",
+                                params={'url': url})
+        self.assertEqual(response.status_code, 404)
